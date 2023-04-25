683d227ab980d7af383cb4cb05bdb254289413a3
jjjjjjy Z <3355951477@qq.com>
2023-04-25 13:12:11 +0800

    first

---
+++ service/service_user/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.preBook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
