43cc8c7804a1d3ca8a8a0de6963b8ef6fdbd48ae
jjjjjjy Z <3355951477@qq.com>
2023-04-27 09:16:15 +0800

    swaggerTure

--- service/service_hosp/src/main/java/com/preBook/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/preBook/ServiceHospApplication.java
@@ -2,8 +2,12 @@ package com.preBook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+
+// http://localhost:3536/swagger-ui.html
 @SpringBootApplication
+//@ComponentScan(basePackages = "com.preBook")
 public class ServiceHospApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceHospApplication.class, args);
--- service/service_hosp/src/main/java/com/preBook/controller/HospitalSetController.java
+++ service/service_hosp/src/main/java/com/preBook/controller/HospitalSetController.java
@@ -3,30 +3,51 @@ package com.preBook.controller;
 
 //import com.preBook.service.HospitalSetService;
 import com.atguigu.yygh.model.hosp.HospitalSet;
+import com.preBook.Result;
 import com.preBook.mapper.HospitalSetMapper;
 import com.preBook.service.HospitalSetService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-//
+
+//http://localhost:3536/admin/hosp/hospitalSet/findAll
+
+@Api(tags = "医院设置管理")
 @RestController
 @RequestMapping("/admin/hosp/hospitalSet")
 public class HospitalSetController {
     @Autowired
     private HospitalSetService hospitalSetService;
 
-//    1.
+//    1.查all
+    @ApiOperation(value = "获取分页列表")
     @GetMapping("findAll")
-    public List<HospitalSet> findAllHospital(){
+    public Result findAllHospital(){
         List<HospitalSet> list = hospitalSetService.list();
-        return list;
+        return Result.ok(list);
     }
 
-    @GetMapping("/hello")
+    /*@GetMapping("/hello")
     public String hello(){
         return "Hello World";
+    }*/
+
+    // 2、逻辑删除
+    @ApiOperation(value = "医院设置逻辑删除")
+    @DeleteMapping("{id}")
+    public Result removeHospSet(@PathVariable Long id) {
+        // 写long都不行，一定要写Long
+        boolean flag = hospitalSetService.removeById(id);
+        if(flag) {
+            return Result.ok();
+        } else {
+            return Result.fail();
+        }
     }
+
+    // 3. 条件查询带分页医院设置
+
 }
