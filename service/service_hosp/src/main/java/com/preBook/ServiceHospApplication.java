8ffe264d01f232ba80cfc7c8fab5e7b2fcc8c00d
jjjjjjy Z <3355951477@qq.com>
2023-04-29 23:58:14 +0800

    blockRedis

--- service/service_hosp/src/main/java/com/preBook/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/preBook/ServiceHospApplication.java
@@ -2,12 +2,15 @@ package com.preBook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
 
 // http://localhost:3536/swagger-ui.html
 @SpringBootApplication
-//@ComponentScan(basePackages = "com.preBook")
+//@EnableCaching
+//@ComponentScan
+// (basePackages = "com.preBook")
 public class ServiceHospApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceHospApplication.class, args);
